d2a81cb5433599fe31873ae0a3d5da5328bcace0
emmadal <emmanueldalougou@gmail.com>
2025-05-02 15:14:54 +0000

    refactor: refactor flag to use go version

--- cmd/use.go
+++ cmd/use.go
@@ -2,10 +2,11 @@ package cmd
 
 import (
 	"fmt"
+	"github.com/emmadal/govm/pkg"
 	"os"
+	"path/filepath"
 	"strings"
 
-	"github.com/emmadal/govm/pkg"
 	"github.com/spf13/cobra"
 )
 
@@ -15,33 +16,42 @@ var useCmd = &cobra.Command{
 	Short:   "Use a specific Go version",
 	Example: strings.Join([]string{"$ govm use 1.21.0"}, "\n"),
 	Args: func(cmd *cobra.Command, args []string) error {
+		if strings.Contains(args[0], "go") {
+			return fmt.Errorf("invalid version format. Please enter a valid version")
+		}
 		if len(args) > 1 || len(args) == 0 {
 			return fmt.Errorf("expect one argument")
 		}
 		return nil
 	},
 	RunE: func(cmd *cobra.Command, args []string) error {
-		err := verifyFileExists(args[0])
+		// Use the Go version
+		tarball := pkg.Tarball{}
+		binary := pkg.Binary{}
+
+		// Get the cached Go version
+		if err := binary.CachedGoVersion(args[0]); err != nil {
+			return err
+		}
+		
+		// Check if the version exists before proceeding
+		folder := filepath.Join(binary.InstallDir, fmt.Sprintf("go%s", args[0]))
+		fileInfo, err := os.Stat(folder)
 		if err != nil {
 			return err
 		}
-		err = pkg.UseGoVersion(args[0])
-		if err != nil {
-			return err
+
+		// use the Go version
+		if fileInfo.IsDir() && fileInfo.Size() > 0 {
+			directory := pkg.Directory{}
+			if err := directory.GetDirectories(); err != nil {
+				return err
+			}
+			// to Use the Go version
+			if err := tarball.UseGoVersion(args[0], directory.ConfigDir); err != nil {
+				return err
+			}
 		}
 		return nil
 	},
-}
-
-// verifyFileExists checks if a file exists
-func verifyFileExists(version string) error {
-	file, err := pkg.CachedGoVersion(version)
-	if err != nil {
-		message := fmt.Sprintf("go%s is not installed. Please do 'govm install %s' first.", version, version)
-		return fmt.Errorf("%s%s%s", pkg.Red_ANSI, message, pkg.Reset_ANSI)
-	}
-	if _, err := os.Stat(file); err != nil {
-		return err
-	}
-	return nil
 }
